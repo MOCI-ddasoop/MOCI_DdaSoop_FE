c75999b13d69f0de955d7e1588e128db363d830b
yoon5450 <dbseodnd356@naver.com>
2025-11-21 13:45:38 +0900

    Feat: 모달을 닫더라도 스크롤 위치가 남아있던 오류 제거

--- src/domain/my-page/components/FeedModal.tsx
+++ src/domain/my-page/components/FeedModal.tsx
@@ -1,30 +1,29 @@
 "use client";
-import { Activity, useEffect } from "react";
+import { Activity, useEffect, useRef } from "react";
 import Image from "next/image";
 import { createPortal } from "react-dom";
 import FeedDetailCard from "./FeedDetailCard";
 import CommentContainer from "./CommentContainer";
 
-const FEED_DATA = [
-  {
-    id: 1,
-    title: "Feed 1",
-    author: "author 1",
-    content: "Feed 1 content",
-    category: "category 1",
-    date: "2025-01-01",
-    likeCount: 10,
-    commentCount: 10,
-    tags: ["tag1", "tag2", "tag3"],
-    likedByMe: false,
-    image:
-      "https://i.namu.wiki/i/bjhl-c4YuugxXFaZaFRbPvU0IGuNuSPvqVoujzeccTdP39XErpUKxRO6HWPeNj9CWIMe_mEnEj5xfuZgB8BrNawRlD1X9gSxsHJsBSVf82G71Mdw4OROpopv0sa4SwRyDINrp08r3mD9WPCv1Xpsow.webp",
-  },
-];
+const FEED_DATA = Array.from({ length: 20 }).map((_, index) => ({
+  id: 1,
+  title: "Feed 1",
+  author: "author 1",
+  content: "Feed 1 content",
+  category: "category 1",
+  date: "2025-01-01",
+  likeCount: 10,
+  commentCount: 10,
+  tags: ["tag1", "tag2", "tag3"],
+  likedByMe: false,
+  image:
+    "https://i.namu.wiki/i/bjhl-c4YuugxXFaZaFRbPvU0IGuNuSPvqVoujzeccTdP39XErpUKxRO6HWPeNj9CWIMe_mEnEj5xfuZgB8BrNawRlD1X9gSxsHJsBSVf82G71Mdw4OROpopv0sa4SwRyDINrp08r3mD9WPCv1Xpsow.webp",
+}));
 
 const COMMENT_LIST = Array.from({ length: 10 }).map((_, index) => ({
   id: index,
-  profileImage:"https://i.namu.wiki/i/bjhl-c4YuugxXFaZaFRbPvU0IGuNuSPvqVoujzeccTdP39XErpUKxRO6HWPeNj9CWIMe_mEnEj5xfuZgB8BrNawRlD1X9gSxsHJsBSVf82G71Mdw4OROpopv0sa4SwRyDINrp08r3mD9WPCv1Xpsow.webp",
+  profileImage:
+    "https://i.namu.wiki/i/bjhl-c4YuugxXFaZaFRbPvU0IGuNuSPvqVoujzeccTdP39XErpUKxRO6HWPeNj9CWIMe_mEnEj5xfuZgB8BrNawRlD1X9gSxsHJsBSVf82G71Mdw4OROpopv0sa4SwRyDINrp08r3mD9WPCv1Xpsow.webp",
   author: "John Doe",
   date: "2025-01-01",
   content: "Hello, world!",
@@ -39,23 +38,31 @@ function FeedModal({
   onClose: () => void;
   isOpen: boolean;
 }) {
-  console.log(feedId);
-  // query를 여기서 조회하고 모달에 출력
+  const contentRef = useRef<HTMLDivElement>(null);
+
+  const handleClose = () => {
+    onClose();
+    contentRef.current?.scrollTo({ top: 0 });
+  };
+
+  // TODO: query를 여기서 조회하고 모달에 출력
+
 
   useEffect(() => {
+    if (!isOpen) return;
+
     // ESC 키로 닫기
     const handleEscape = (e: KeyboardEvent) => {
-      if (e.key === "Escape") onClose();
+      if (e.key === "Escape") handleClose();
     };
     document.addEventListener("keydown", handleEscape);
-    // body 스크롤 막기
     document.body.style.overflow = "hidden";
 
     return () => {
       document.removeEventListener("keydown", handleEscape);
       document.body.style.overflow = "unset";
     };
-  }, [onClose]);
+  }, [isOpen]);
 
   if (typeof window === "undefined") return null;
 
@@ -63,7 +70,7 @@ function FeedModal({
     <Activity mode={isOpen ? "visible" : "hidden"}>
       <div
         className="fixed top-0 left-0 w-full h-full bg-black/50 z-50 flex items-center justify-center backdrop-blur-sm"
-        onClick={onClose}
+        onClick={handleClose}
       >
         <div
           className="flex w-full h-[90vh] max-w-4xl max-h-4xl bg-white rounded-md"
@@ -80,7 +87,7 @@ function FeedModal({
           </div>
 
           {/* content 영역 */}
-          <div className="w-2/5 h-full overflow-y-auto">
+          <div ref={contentRef} className="w-2/5 h-full overflow-y-auto">
             <FeedDetailCard
               id={FEED_DATA[Number(feedId)].id}
               title={FEED_DATA[Number(feedId)].title}
@@ -98,8 +105,6 @@ function FeedModal({
             {/* comment 영역 */}
             <CommentContainer commentList={COMMENT_LIST} />
           </div>
-
-          
         </div>
       </div>
     </Activity>,
