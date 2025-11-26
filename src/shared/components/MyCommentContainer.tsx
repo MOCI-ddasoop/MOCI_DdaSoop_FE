6bcc52c0bf8047c94234e438997ad5acdbc1c0df
yoon5450 <dbseodnd356@naver.com>
2025-11-26 21:37:35 +0900

    Refactor: 공용 컴포넌트로 Input 요소 변경

---
+++ src/shared/components/MyCommentContainer.tsx
@@ -0,0 +1,10 @@
+import MyCommentItem from "./MyCommentItem"
+
+function MyCommentContainer() {
+  return (
+    <ul className="flex flex-col gap-4">
+      <MyCommentItem />
+    </ul>
+  )
+}
+export default MyCommentContainer
