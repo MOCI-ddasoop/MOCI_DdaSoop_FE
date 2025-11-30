aa8d60789f50f29e136102877dc51f3f07aa5cfd
Young-mang <fgb9763@gmail.com>
2025-11-30 18:50:06 +0900

    [Fix] merge 충돌 해결

--- src/app/mypage/page.tsx
+++ src/app/mypage/page.tsx
@@ -1,7 +1,19 @@
-import { redirect } from "next/navigation";
+import CardContainer from "@/domain/mypage/components/CardContainer";
 
-function page() {
-  redirect("/mypage/feeds");
-  
+import { mypageTabContents } from "@/utils/navigation";
+import UserInfo from "@/domain/mypage/components/UserInfo";
+import TabBar from "@/shared/components/TabBar";
+
+function TestPage() {
+	return (
+		<div className="w-full h-full flex flex-col items-center justify-center">
+			<div className="w-full h-full flex flex-col items-center justify-center max-w-250 py-10 gap-10">
+				<UserInfo />
+				<TabBar tabContents={mypageTabContents} type="mypage" />
+				<CardContainer className="px-4" />
+			</div>
+		</div>
+	);
 }
-export default page
+
+export default TestPage;
