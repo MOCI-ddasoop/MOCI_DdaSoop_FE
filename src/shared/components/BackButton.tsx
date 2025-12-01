a144a93812499c511554dffecb386172401021a2
yoon5450 <dbseodnd356@naver.com>
2025-12-01 12:42:38 +0900

    Feat: 공용 컴포넌트 BackButton 추가, 404 페이지

---
+++ src/shared/components/BackButton.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import tw from "../utils/tw";
+
+function BackButton({
+  className,
+  children,
+}: {
+  className?: string;
+  children?: React.ReactNode;
+}) {
+  const router = useRouter();
+
+  return (
+    <button
+      type="button"
+      onClick={() => router.back()}
+      className={tw("text-white px-4 py-2 rounded-md", className)}
+    >
+      {children}
+    </button>
+  );
+}
+export default BackButton;
