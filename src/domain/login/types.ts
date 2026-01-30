2a7c3e8f536bd70fb3ed2b8ab330050aba672f46
seungdev <trsemaris@gmail.com>
2026-01-30 13:26:57 +0900

    [Feat] 로그인 타입 추가

---
+++ src/domain/login/types.ts
@@ -0,0 +1,7 @@
+import type { components } from "@/types/api/v1";
+
+export type NicknameCheckResponse =
+  components["schemas"]["NicknameCheckResponse"];
+
+export type EmailCheckResponse =
+  components["schemas"]["EmailCheckResponse"];
