8772c76fd9291ae55d0886c2c1edabcc88e6b857
sujin209 <20201036@sungshin.ac.kr>
2026-02-02 18:24:28 +0900

    [Refactor]suspense 추가

--- src/app/(default)/together/page.tsx
+++ src/app/(default)/together/page.tsx
@@ -1,13 +1,14 @@
 import { getInitTogetherList } from "@/domain/together/api/getInitTogetherList";
 import TogetherSection from "@/domain/together/components/TogetherSection";
+import { Suspense } from "react";
 
 async function Together() {
   const initialData = await getInitTogetherList();
 
   return (
-    <>
+    <Suspense fallback={<div>Loading...</div>}>
       <TogetherSection initialData={initialData} />
-    </>
+    </Suspense>
   );
 }
 
