e3ca2bde66b761dfa29002995d34ef0f0d2f09a4
Young-mang <fgb9763@gmail.com>
2026-02-23 02:48:17 +0900

    fix: 이전 feat(피드 생성시 함께하기 선택 옵션 활성화) 커밋 누락 파일 보완

--- src/domain/main/components/TogetherList.tsx
+++ src/domain/main/components/TogetherList.tsx
@@ -3,30 +3,29 @@ import { TogetherListItemProps } from "@/domain/together/types";
 import Link from "next/link";
 
 function TogetherList({ items }: { items: TogetherListItemProps[] }) {
-  return (
-    <div className="w-62 h-fit p-3 shadow-md rounded-lg flex flex-col gap-3">
-      <div className="flex justify-between items-center">
-        <h3>함께하기</h3>
-        <Link
-          href="/together"
-          className="text-sm hover:underline hover:underline-offset-2"
-        >
-          더보기
-        </Link>
-      </div>
-      {items.map(({ id, image, name, category, isOnline, href }) => (
-        <TogetherListItem
-          key={id}
-          id={id}
-          image={image}
-          name={name}
-          category={category}
-          isOnline={isOnline}
-          href={href}
-        />
-      ))}
-    </div>
-  );
+	return (
+		<div className="w-62 h-fit p-3 shadow-md rounded-lg flex flex-col gap-3">
+			<div className="flex justify-between items-center">
+				<h3>함께하기</h3>
+				<Link
+					href="/together"
+					className="text-sm hover:underline hover:underline-offset-2"
+				>
+					더보기
+				</Link>
+			</div>
+			{items.map(({ id, image, name, category, isOnline }) => (
+				<TogetherListItem
+					key={id}
+					id={id}
+					image={image}
+					name={name}
+					category={category}
+					isOnline={isOnline}
+				/>
+			))}
+		</div>
+	);
 }
 
 export default TogetherList;
