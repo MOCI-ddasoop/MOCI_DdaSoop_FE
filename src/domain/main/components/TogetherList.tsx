4f0e038a440453a56a1b2d7f5220928c2dc5eebd
sujin209 <20201036@sungshin.ac.kr>
2026-02-24 13:02:05 +0900

    [Feat]메인페이지 후원하기 이동 링크 추가

--- src/domain/main/components/TogetherList.tsx
+++ src/domain/main/components/TogetherList.tsx
@@ -4,37 +4,38 @@ import { categoryType, isOnlineType } from "@/shared/constants/filter";
 import Link from "next/link";
 
 function TogetherList({
-	items,
-	type,
+  items,
+  type,
 }: {
-	items: TogetherInfo[];
-	type: string;
+  items: TogetherInfo[];
+  type: string;
 }) {
-	return (
-		<div className="w-62 h-fit p-3 shadow-md rounded-lg flex flex-col gap-3">
-			<div className="flex justify-between items-center">
-				<h3>
-					<span className="text-mainblue font-medium">{type}</span> 함께하기
-				</h3>
-				<Link
-					href="/together"
-					className="text-sm hover:underline hover:underline-offset-2"
-				>
-					더보기
-				</Link>
-			</div>
-			{items.map(({ id, thumbnailImage, title, category, mode }) => (
-				<TogetherListItem
-					key={id}
-					id={id}
-					image={thumbnailImage ?? "/defaultFeedImage.png"}
-					name={title}
-					category={categoryType[category]}
-					isOnline={isOnlineType[mode]}
-				/>
-			))}
-		</div>
-	);
+  return (
+    <div className="w-62 h-fit p-3 shadow-md rounded-lg flex flex-col gap-3">
+      <div className="flex justify-between items-center">
+        <h3>
+          <span className="text-mainblue font-medium">{type}</span> 함께하기
+        </h3>
+        <Link
+          href="/together"
+          className="text-sm hover:underline hover:underline-offset-2"
+        >
+          더보기
+        </Link>
+      </div>
+      {items.map(({ id, thumbnailImage, title, category, mode }) => (
+        <TogetherListItem
+          key={id}
+          id={id}
+          image={thumbnailImage ?? "/defaultFeedImage.png"}
+          name={title}
+          category={categoryType[category]}
+          isOnline={isOnlineType[mode]}
+          href={`/together/${id}`}
+        />
+      ))}
+    </div>
+  );
 }
 
 export default TogetherList;
