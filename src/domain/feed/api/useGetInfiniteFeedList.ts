88d3e08ef8c2fe45799e3722d09a14144ae16e3d
Young-mang <fgb9763@gmail.com>
2026-02-27 11:55:07 +0900

    fix: memberId 유무에 따른 feed/scroll 캐시 분리 처리

--- src/domain/feed/api/useGetInfiniteFeedList.ts
+++ src/domain/feed/api/useGetInfiniteFeedList.ts
@@ -3,113 +3,113 @@ import { api } from "@/shared/config/api";
 import { queryKeys } from "@/shared/config/queryKeys";
 
 export type InfiniteFeedParams =
-  | { page?: undefined; query?: string }
-  | {
-      page: "together";
-      togetherId: number;
-      notice?: boolean;
-    }
-  | {
-      page: "member";
-      memberId: number;
-      bookmark?: boolean;
-    };
+	| { page?: undefined; query?: string; memberId?: number }
+	| {
+			page: "together";
+			togetherId: number;
+			notice?: boolean;
+	  }
+	| {
+			page: "member";
+			memberId: number;
+			bookmark?: boolean;
+	  };
 
 export const useGetInfiniteFeedList = (
-  infiniteParams: InfiniteFeedParams = {},
+	infiniteParams: InfiniteFeedParams = {},
 ) => {
-  return useInfiniteQuery({
-    queryKey: (() => {
-      if (infiniteParams.page === "together") {
-        return infiniteParams.notice
-          ? queryKeys.feeds.togetherNotice(infiniteParams.togetherId)
-          : queryKeys.feeds.infinite({
-              page: "together",
-              togetherId: infiniteParams.togetherId,
-            });
-      }
-      if (infiniteParams.page === "member") {
-        return infiniteParams.bookmark
-          ? queryKeys.feeds.bookmark()
-          : queryKeys.feeds.infinite({
-              page: "member",
-              memberId: infiniteParams.memberId,
-            });
-      }
-      if (
-        infiniteParams.query &&
-        // #만 입력한 경우 빼고
-        !(
-          infiniteParams.query.startsWith("#") &&
-          infiniteParams.query.slice(1).trim() === ""
-        )
-      ) {
-        return queryKeys.feeds.search(infiniteParams.query.trim());
-      }
-      return queryKeys.feeds.infinite();
-    })(),
-    queryFn: async ({ pageParam }) => {
-      let baseUrl;
+	return useInfiniteQuery({
+		queryKey: (() => {
+			if (infiniteParams.page === "together") {
+				return infiniteParams.notice
+					? queryKeys.feeds.togetherNotice(infiniteParams.togetherId)
+					: queryKeys.feeds.infinite({
+							page: "together",
+							togetherId: infiniteParams.togetherId,
+						});
+			}
+			if (infiniteParams.page === "member") {
+				return infiniteParams.bookmark
+					? queryKeys.feeds.bookmark()
+					: queryKeys.feeds.infinite({
+							page: "member",
+							memberId: infiniteParams.memberId,
+						});
+			}
+			if (
+				infiniteParams.query &&
+				// #만 입력한 경우 빼고
+				!(
+					infiniteParams.query.startsWith("#") &&
+					infiniteParams.query.slice(1).trim() === ""
+				)
+			) {
+				return queryKeys.feeds.search(infiniteParams.query.trim());
+			}
+			return queryKeys.feeds.infinite({ memberId: infiniteParams.memberId });
+		})(),
+		queryFn: async ({ pageParam }) => {
+			let baseUrl;
 
-      switch (infiniteParams.page) {
-        case "together":
-          baseUrl = infiniteParams.notice
-            ? `/api/feeds/together/${infiniteParams.togetherId}/notices`
-            : `/api/feeds/together/${infiniteParams.togetherId}/scroll`;
-          break;
+			switch (infiniteParams.page) {
+				case "together":
+					baseUrl = infiniteParams.notice
+						? `/api/feeds/together/${infiniteParams.togetherId}/notices`
+						: `/api/feeds/together/${infiniteParams.togetherId}/scroll`;
+					break;
 
-        case "member":
-          baseUrl = infiniteParams.bookmark
-            ? `/api/feeds/bookmarks/me`
-            : `/api/feeds/members/${infiniteParams.memberId}/scroll`;
-          break;
+				case "member":
+					baseUrl = infiniteParams.bookmark
+						? `/api/feeds/bookmarks/me`
+						: `/api/feeds/members/${infiniteParams.memberId}/scroll`;
+					break;
 
-        default:
-          if (infiniteParams.query) {
-            if (infiniteParams.query.startsWith("#")) {
-              // 태그검색
-              baseUrl =
-                infiniteParams.query.slice(1).trim() !== ""
-                  ? "/api/feeds/search/tag"
-                  : "/api/feeds/scroll"; // 태그입력안했으면 전체무한스크롤
-            } else baseUrl = "/api/feeds"; // 그냥검색
-          } else {
-            baseUrl = "/api/feeds/scroll"; // 검색아니면 전체무한스크롤
-          }
-      }
-      const params: {
-        size: number;
-        tag?: string;
-        keyword?: string;
-        lastFeedId?: number;
-        togetherId?: number;
-        memberId?: number;
-      } = { size: 20 };
+				default:
+					if (infiniteParams.query) {
+						if (infiniteParams.query.startsWith("#")) {
+							// 태그검색
+							baseUrl =
+								infiniteParams.query.slice(1).trim() !== ""
+									? "/api/feeds/search/tag"
+									: "/api/feeds/scroll"; // 태그입력안했으면 전체무한스크롤
+						} else baseUrl = "/api/feeds"; // 그냥검색
+					} else {
+						baseUrl = "/api/feeds/scroll"; // 검색아니면 전체무한스크롤
+					}
+			}
+			const params: {
+				size: number;
+				tag?: string;
+				keyword?: string;
+				lastFeedId?: number;
+				togetherId?: number;
+				memberId?: number;
+			} = { size: 20 };
 
-      if (pageParam !== undefined && pageParam !== null) {
-        params.lastFeedId = pageParam;
-      }
-      if (
-        infiniteParams.page !== "together" &&
-        infiniteParams.page !== "member"
-      ) {
-        if (infiniteParams.query?.startsWith("#")) {
-          params.tag = infiniteParams.query.slice(1).trim() || undefined;
-        } else if (infiniteParams.query) {
-          params.keyword = infiniteParams.query.trim();
-        }
-      }
+			if (pageParam !== undefined && pageParam !== null) {
+				params.lastFeedId = pageParam;
+			}
+			if (
+				infiniteParams.page !== "together" &&
+				infiniteParams.page !== "member"
+			) {
+				if (infiniteParams.query?.startsWith("#")) {
+					params.tag = infiniteParams.query.slice(1).trim() || undefined;
+				} else if (infiniteParams.query) {
+					params.keyword = infiniteParams.query.trim();
+				}
+			}
 
-      const res =
-        infiniteParams.page === "together" && infiniteParams.notice
-          ? await api.get(baseUrl)
-          : await api.get(baseUrl, { params });
-      return res.data;
-    },
-    getNextPageParam: (lastPage) => {
-      if (!lastPage.hasNext) return undefined;
-      return lastPage.nextCursor;
-    },
-    initialPageParam: undefined,
-  });
+			const res =
+				infiniteParams.page === "together" && infiniteParams.notice
+					? await api.get(baseUrl)
+					: await api.get(baseUrl, { params });
+			return res.data;
+		},
+		getNextPageParam: (lastPage) => {
+			if (!lastPage.hasNext) return undefined;
+			return lastPage.nextCursor;
+		},
+		initialPageParam: undefined,
+	});
 };
--- src/shared/config/queryKeys.ts
+++ src/shared/config/queryKeys.ts
@@ -1,126 +1,126 @@
 import { isCancel } from "axios";
 
 export const queryKeys = {
-  feeds: {
-    all: () => ["feeds"],
-    id: (id: string | number) => [
-      "feeds",
-      typeof id === "string" ? id : String(id),
-    ],
-    list: (pageNum: number | string, params?: string[]) => [
-      "feeds",
-      "list",
-      pageNum,
-      params,
-    ],
-    infinite: (params?: {
-      query?: string;
-      page?: "together" | "member";
-      togetherId?: number;
-      memberId?: number;
-    }) => {
-      if (!params?.page) {
-        return ["feeds", "infinite"];
-      }
+	feeds: {
+		all: () => ["feeds"],
+		id: (id: string | number) => [
+			"feeds",
+			typeof id === "string" ? id : String(id),
+		],
+		list: (pageNum: number | string, params?: string[]) => [
+			"feeds",
+			"list",
+			pageNum,
+			params,
+		],
+		infinite: (params?: {
+			query?: string;
+			page?: "together" | "member";
+			togetherId?: number;
+			memberId?: number;
+		}) => {
+			if (!params?.page) {
+				return ["feeds", "infinite", { memberId: params?.memberId }];
+			}
 
-      if (params.page === "together") {
-        return [
-          "feeds",
-          "infinite",
-          { page: "together", togetherId: params.togetherId },
-        ];
-      }
+			if (params.page === "together") {
+				return [
+					"feeds",
+					"infinite",
+					{ page: "together", togetherId: params.togetherId },
+				];
+			}
 
-      return [
-        "feeds",
-        "infinite",
-        { page: "member", memberId: params.memberId },
-      ];
-    },
-    togetherNotice: (id: number) => ["feeds", "notice", id],
-    bookmark: () => ["feeds", "bookmark"],
-    search: (query?: string) => ["feeds", "infinite", "search", query],
+			return [
+				"feeds",
+				"infinite",
+				{ page: "member", memberId: params.memberId },
+			];
+		},
+		togetherNotice: (id: number) => ["feeds", "notice", id],
+		bookmark: () => ["feeds", "bookmark"],
+		search: (query?: string) => ["feeds", "infinite", "search", query],
 
-    reaction: () => ["feeds", "reaction"],
-    del: () => ["feeds", "delete"],
-    udt: () => ["feeds", "update"],
-  },
-  comments: {
-    all: () => ["comments"],
-    id: (id: number) => ["comments", id],
-    list: (feedId: string) => ["comments", feedId],
-    react: () => ["comments", "react"],
-    listByUser: (userId: string) => ["comments", "listByUser", userId],
-    del: () => ["comments", "delete"],
-    upt: () => ["comments", "update"],
-  },
+		reaction: () => ["feeds", "reaction"],
+		del: () => ["feeds", "delete"],
+		udt: () => ["feeds", "update"],
+	},
+	comments: {
+		all: () => ["comments"],
+		id: (id: number) => ["comments", id],
+		list: (feedId: string) => ["comments", feedId],
+		react: () => ["comments", "react"],
+		listByUser: (userId: string) => ["comments", "listByUser", userId],
+		del: () => ["comments", "delete"],
+		upt: () => ["comments", "update"],
+	},
 
-  donate: {
-    id: (id: string) => ["donate", id],
-    list: (params: {
-      category?: string[];
-      status?: string;
-      sortType?: string;
-      page?: number;
-      size?: number;
-    }) => ["donate", "list", params],
-    new: () => ["donate", "new"],
-    history: (id: string) => ["donate", "history", id],
-    newsCreate: (id: string) => ["donate", "news", "new", id],
-    news: (id: string) => ["donate", "news", id],
-    member: (memberId: number) => ["donate", "member", memberId],
-    memberHistory: (memberId: number) => [
-      "donate",
-      "member",
-      "history",
-      memberId,
-    ],
-    summary: () => ["donate", "summary"],
-    isCreator: ({ id, memberId }: { id: string; memberId: number }) => [
-      "donate",
-      "isCreator",
-      { id, memberId },
-    ],
-  },
-  together: {
-    id: (id: string) => ["together", id],
-    list: (params: {
-      category?: string[];
-      mode?: string;
-      status?: string;
-      sortType?: string;
-      page?: number;
-      size?: number;
-    }) => ["together", "list", params],
-    description: (id: string | number) => ["together", "description", id],
-    member: (memberId: number) => ["together", "member", memberId],
-    new: () => ["together", "new"],
-    join: () => ["together", "join"],
-    leave: () => ["together", "leave"],
-    isParticipating: () => ["together", "participating"],
-  },
+	donate: {
+		id: (id: string) => ["donate", id],
+		list: (params: {
+			category?: string[];
+			status?: string;
+			sortType?: string;
+			page?: number;
+			size?: number;
+		}) => ["donate", "list", params],
+		new: () => ["donate", "new"],
+		history: (id: string) => ["donate", "history", id],
+		newsCreate: (id: string) => ["donate", "news", "new", id],
+		news: (id: string) => ["donate", "news", id],
+		member: (memberId: number) => ["donate", "member", memberId],
+		memberHistory: (memberId: number) => [
+			"donate",
+			"member",
+			"history",
+			memberId,
+		],
+		summary: () => ["donate", "summary"],
+		isCreator: ({ id, memberId }: { id: string; memberId: number }) => [
+			"donate",
+			"isCreator",
+			{ id, memberId },
+		],
+	},
+	together: {
+		id: (id: string) => ["together", id],
+		list: (params: {
+			category?: string[];
+			mode?: string;
+			status?: string;
+			sortType?: string;
+			page?: number;
+			size?: number;
+		}) => ["together", "list", params],
+		description: (id: string | number) => ["together", "description", id],
+		member: (memberId: number) => ["together", "member", memberId],
+		new: () => ["together", "new"],
+		join: () => ["together", "join"],
+		leave: () => ["together", "leave"],
+		isParticipating: () => ["together", "participating"],
+	},
 
-  notifications: {
-    list: (type?: string) =>
-      type ? ["notifications", "list", type] : ["notifications", "list"],
+	notifications: {
+		list: (type?: string) =>
+			type ? ["notifications", "list", type] : ["notifications", "list"],
 
-    page: (type: string, pageNum: number | string, size?: number) => [
-      "notifications",
-      "list",
-      type,
-      pageNum,
-      size,
-    ],
-    recent: ["notifications", "recent"],
-    read: () => ["notifications", "read"],
-    delete: () => ["notifications", "delete"],
-  },
+		page: (type: string, pageNum: number | string, size?: number) => [
+			"notifications",
+			"list",
+			type,
+			pageNum,
+			size,
+		],
+		recent: ["notifications", "recent"],
+		read: () => ["notifications", "read"],
+		delete: () => ["notifications", "delete"],
+	},
 
-  members: {
-    me: () => ["members", "me"],
-    counts: () => ["members", "counts"],
-  },
-  report: {
-    create: () => ["report", "create"],
-  },
+	members: {
+		me: () => ["members", "me"],
+		counts: () => ["members", "counts"],
+	},
+	report: {
+		create: () => ["report", "create"],
+	},
 };
