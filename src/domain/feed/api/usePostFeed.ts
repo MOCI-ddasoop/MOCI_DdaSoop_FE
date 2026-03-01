0e153f588fde0eabba1f542aa3c8564100815ca7
sujin209 <20201036@sungshin.ac.kr>
2026-03-01 21:32:10 +0900

    [Feat]피드 수정/삭제/좋아요/업로드 시 쿼리키 관련 수정

--- src/domain/feed/api/usePostFeed.ts
+++ src/domain/feed/api/usePostFeed.ts
@@ -1,22 +1,39 @@
 import { api } from "@/shared/config/api";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { FeedCreateRequest } from "../types";
+import { queryKeys } from "@/shared/config/queryKeys";
+import { useAuthStore } from "@/store/authStore";
 
 export const usePostFeed = (options?: Parameters<typeof useMutation>[0]) => {
-	const qc = useQueryClient();
+  const qc = useQueryClient();
+  const memberId = useAuthStore((s) => s.me?.memberId);
 
-	return useMutation({
-		...options,
-		mutationFn: async (feedData: FeedCreateRequest) => {
-			const { data } = await api.post("api/feeds", feedData);
-			return data;
-		},
-		onSuccess: (data, variables, context, mutation) => {
-			qc.invalidateQueries({
-				queryKey: ["feeds", "infinite"],
-			});
+  return useMutation({
+    ...options,
+    mutationFn: async (feedData: FeedCreateRequest) => {
+      const { data } = await api.post("api/feeds", feedData);
+      return data;
+    },
+    onSuccess: (data, variables, context, mutation) => {
+      if (variables.feedType === "TOGETHER_NOTICE")
+        qc.invalidateQueries({
+          queryKey: queryKeys.feeds.togetherNotice(variables.togetherId!),
+        });
+      qc.invalidateQueries({
+        predicate: (query) => {
+          const key = query.queryKey as string[];
+          return (
+            key[0] === "feeds" &&
+            key[1] === "infinite" &&
+            typeof key[2] !== "number"
+          );
+        },
+      });
+      qc.invalidateQueries({
+        queryKey: ["feeds", "infinite", memberId],
+      });
 
-			options?.onSuccess?.(data, variables, context, mutation);
-		},
-	});
+      options?.onSuccess?.(data, variables, context, mutation);
+    },
+  });
 };
--- src/domain/feed/api/useUdtFeedById.ts
+++ src/domain/feed/api/useUdtFeedById.ts
@@ -1,47 +1,80 @@
 import { queryKeys } from "@/shared/config/queryKeys";
 import {
-	InfiniteData,
-	useMutation,
-	useQueryClient,
+  InfiniteData,
+  useMutation,
+  useQueryClient,
 } from "@tanstack/react-query";
-import { FeedInfiniteScroll, FeedUpdateRequest } from "../types";
+import { FeedInfinite, FeedInfiniteScroll, FeedUpdateRequest } from "../types";
 import { api } from "@/shared/config/api";
+import { useAuthStore } from "@/store/authStore";
+import { CommentPage } from "@/domain/comment/types";
 
-export const useUpdateFeedById = () => {
-	const qc = useQueryClient();
+export const useUpdateFeedById = ({ togetherId }: { togetherId?: number }) => {
+  const qc = useQueryClient();
+  const memberId = useAuthStore((s) => s.me?.memberId);
 
-	return useMutation({
-		mutationKey: queryKeys.feeds.udt(),
-		mutationFn: async ({
-			id,
-			content,
-		}: {
-			id: number;
-			content: FeedUpdateRequest;
-		}) => {
-			const res = await api.put(`api/feeds/${id}`, content);
-			return res;
-		},
-		onSuccess: (res, { id }) => {
-			const updatedFeed = res.data;
-			qc.invalidateQueries({
-				queryKey: queryKeys.feeds.id(id),
-			});
-			qc.setQueriesData<InfiniteData<FeedInfiniteScroll>>(
-				{ queryKey: ["feeds", "infinite"] },
-				(oldData) => {
-					if (!oldData) return oldData;
-					return {
-						...oldData,
-						pages: oldData.pages.map((page) => ({
-							...page,
-							content: page.content.map((feed) =>
-								feed.id === id ? updatedFeed : feed,
-							),
-						})),
-					};
-				},
-			);
-		},
-	});
+  return useMutation({
+    mutationKey: queryKeys.feeds.udt(),
+    mutationFn: async ({
+      id,
+      content,
+    }: {
+      id: number;
+      content: FeedUpdateRequest;
+    }) => {
+      const res = await api.put(`api/feeds/${id}`, content);
+      return res;
+    },
+    onSuccess: (res, { id }) => {
+      const updatedFeed = res.data;
+      const updateFn = (
+        oldData: InfiniteData<FeedInfiniteScroll> | undefined,
+      ) => {
+        if (!oldData) return oldData;
+        return {
+          ...oldData,
+          pages: oldData.pages.map((page) => ({
+            ...page,
+            content: page.content.map((feed) =>
+              feed.id === id ? updatedFeed : feed,
+            ),
+          })),
+        };
+      };
+      if (!id) return;
+      qc.refetchQueries({
+        queryKey: queryKeys.feeds.id(id),
+      });
+      qc.setQueriesData<InfiniteData<CommentPage>>(
+        { queryKey: queryKeys.comments.listByUser(String(memberId)) },
+        updateFn,
+      );
+      qc.setQueriesData<InfiniteData<FeedInfiniteScroll>>(
+        {
+          predicate: (query) => {
+            const key = query.queryKey as string[];
+            return (
+              key[0] === "feeds" &&
+              key[1] === "infinite" &&
+              typeof key[2] !== "number"
+            );
+          },
+        },
+        updateFn,
+      );
+      qc.setQueriesData<InfiniteData<FeedInfiniteScroll>>(
+        { queryKey: ["feeds", "infinite", memberId] },
+        updateFn,
+      );
+      if (togetherId) {
+        qc.setQueryData<FeedInfinite[]>(
+          queryKeys.feeds.togetherNotice(togetherId),
+          (oldData) => {
+            if (!oldData) return oldData;
+            return oldData.map((feed) => (feed.id === id ? updatedFeed : feed));
+          },
+        );
+      }
+    },
+  });
 };
