e8176876b57ff462432aac58f19a2d8e92b9376b
sujin209 <20201036@sungshin.ac.kr>
2026-03-11 23:53:54 +0900

    [Fix]함께하기 진행률 및 목표피드개수 관련 수정

--- src/domain/feed/api/usePostFeed.ts
+++ src/domain/feed/api/usePostFeed.ts
@@ -1,8 +1,8 @@
 import { api } from "@/shared/config/api";
 import {
-	useMutation,
-	UseMutationOptions,
-	useQueryClient,
+  useMutation,
+  UseMutationOptions,
+  useQueryClient,
 } from "@tanstack/react-query";
 import { ApiErrorResponse, FeedCreateRequest } from "../types";
 import { queryKeys } from "@/shared/config/queryKeys";
@@ -10,41 +10,45 @@ import { useAuthStore } from "@/store/authStore";
 import { AxiosError } from "axios";
 
 export const usePostFeed = (
-	options?: UseMutationOptions<
-		unknown,
-		AxiosError<ApiErrorResponse>,
-		FeedCreateRequest
-	>,
+  options?: UseMutationOptions<
+    unknown,
+    AxiosError<ApiErrorResponse>,
+    FeedCreateRequest
+  >,
 ) => {
-	const qc = useQueryClient();
-	const memberId = useAuthStore((s) => s.me?.memberId);
+  const qc = useQueryClient();
+  const memberId = useAuthStore((s) => s.me?.memberId);
 
-	return useMutation({
-		...options,
-		mutationFn: async (feedData: FeedCreateRequest) => {
-			const { data } = await api.post("api/feeds", feedData);
-			return data;
-		},
-		onSuccess: (data, variables, context, mutation) => {
-			if (variables.feedType === "TOGETHER_NOTICE")
-				qc.invalidateQueries({
-					queryKey: queryKeys.feeds.togetherNotice(variables.togetherId!),
-				});
-			qc.invalidateQueries({
-				predicate: (query) => {
-					const key = query.queryKey as string[];
-					return (
-						key[0] === "feeds" &&
-						key[1] === "infinite" &&
-						typeof key[2] !== "number"
-					);
-				},
-			});
-			qc.invalidateQueries({
-				queryKey: ["feeds", "infinite", memberId],
-			});
+  return useMutation({
+    ...options,
+    mutationFn: async (feedData: FeedCreateRequest) => {
+      const { data } = await api.post("api/feeds", feedData);
+      return data;
+    },
+    onSuccess: (data, variables, context, mutation) => {
+      if (variables.feedType === "TOGETHER_NOTICE")
+        qc.invalidateQueries({
+          queryKey: queryKeys.feeds.togetherNotice(variables.togetherId!),
+        });
+      if (variables.feedType === "TOGETHER_VERIFICATION")
+        qc.invalidateQueries({
+          queryKey: queryKeys.together.id(String(variables.togetherId!)),
+        });
+      qc.invalidateQueries({
+        predicate: (query) => {
+          const key = query.queryKey as string[];
+          return (
+            key[0] === "feeds" &&
+            key[1] === "infinite" &&
+            typeof key[2] !== "number"
+          );
+        },
+      });
+      qc.invalidateQueries({
+        queryKey: ["feeds", "infinite", memberId],
+      });
 
-			options?.onSuccess?.(data, variables, context, mutation);
-		},
-	});
+      options?.onSuccess?.(data, variables, context, mutation);
+    },
+  });
 };
