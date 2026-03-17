bfd3b7f5d96bf057256f597c9256ef6af256dd8e
sujin209 <20201036@sungshin.ac.kr>
2026-03-17 15:54:35 +0900

    [Fix]배포에서 proxy로 요청보내도록수정

--- src/shared/utils/getServerURL.ts
+++ src/shared/utils/getServerURL.ts
@@ -1,5 +1,5 @@
 export const getServerURL = () => {
-	if (typeof window === "undefined") return "";
-	if (window.location.href.includes("localhost")) return "/proxy-api";
-	else return "";
+  if (typeof window === "undefined") return "";
+  if (window.location.href.includes("localhost")) return "/proxy-api";
+  else return "/proxy-api";
 };
