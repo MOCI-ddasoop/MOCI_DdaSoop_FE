759b924bb52bbfc61a0080f1282081f66f9477a5
sujin209 <20201036@sungshin.ac.kr>
2026-03-25 17:01:44 +0900

    [Fix]후원하기문구수정

--- src/domain/donate/components/DonateModal.tsx
+++ src/domain/donate/components/DonateModal.tsx
@@ -42,7 +42,7 @@ function DonateModal({
   const paymentList = [
     {
       key: "CARD" as const,
-      method: "카드",
+      method: "카드 및 간편결제",
       icon: <FaRegCreditCard size={28} />,
     },
     {
@@ -259,7 +259,7 @@ function DonateModal({
         )}
         {step === "paymethod" && (
           <div className="flex flex-col gap-5 px-10 py-5 w-full">
-            <div className="grid grid-cols-3 gap-4 py-4">
+            <div className="grid grid-cols-2 gap-4 py-4">
               {paymentList.map(({ key, method, icon }, i) => (
                 <Button
                   key={i}
